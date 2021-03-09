10e0d3ee792f0488231ec70ace7cd88213ee9596
Girish Mani <girishkrishna97@gmail.com>
2021-03-09 19:17:52 +0530

    alternate solution using dictionary

--- leetcode_1.py
+++ leetcode_1.py
@@ -8,3 +8,15 @@ class Solution:
             if target - value in nums and nums.index(target-value) != index:
                 return [index, nums.index(target-value)]
         return None
+
+
+'''Commonly found solution using dictionary'''
+class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+      result = {}
+      for index, value in enumerate(nums):
+          if target - value in result:
+              return [index, result[target - value]]
+          else:
+              result[value] = index            
+      return None
