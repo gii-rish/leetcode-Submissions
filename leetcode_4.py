571f5c6e40301143844273c72578f92fec4a591f
Girish Mani <girishkrishna97@gmail.com>
2021-03-12 19:19:51 +0530

    updated comments

--- leetcode_4.py
+++ leetcode_4.py
@@ -1,6 +1,7 @@
-'''Unpack the given lists to a new list, find the length of the new list, divide the length by 2, if it's not divisible\
-then the value in quotient's index is the result, else find the average of the value in quotient's index and of the \
-value in quotient's index -1  '''
+'''Unpack the given lists to a new list, find the length of the new list, divide the length by 2,\
+if it's not divisible then the value in quotient's index is the result, else find the average of \
+the value in quotient's index and of the value in quotient's index -1'''
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         mergedArray = sorted([*nums1, *nums2])
