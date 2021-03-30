1f64d838b3177cb43dec6d4cfdce9a4c0939b0af
Girish Mani <girishkrishna97@gmail.com>
2021-03-30 19:41:52 +0530

    solution for problem 7

---
+++ leetcode_7.py
@@ -0,0 +1,4 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        result = int(str(x)[:0:-1].strip('0'))*-1 if x < 0 else int(str(x)[::-1])
+        return 0 if result.bit_length()>31 else result
