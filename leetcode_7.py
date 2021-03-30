eb969361e2c99fc348923c937ebc13bf81d97974
Girish Mani <girishkrishna97@gmail.com>
2021-03-30 19:44:51 +0530

    added description

--- leetcode_7.py
+++ leetcode_7.py
@@ -1,3 +1,5 @@
+'''return 0 if the reversed value goes outside the signed 32-bit integer range else return
+the reversed number'''
 class Solution:
     def reverse(self, x: int) -> int:
         result = int(str(x)[:0:-1].strip('0'))*-1 if x < 0 else int(str(x)[::-1])
