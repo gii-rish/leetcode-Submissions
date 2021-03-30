6137d51685d00bfd6aa37bd3bc2cee7c8b9cd374
Girish Mani <girishkrishna97@gmail.com>
2021-03-30 19:46:43 +0530

    commit

--- leetcode_7.py
+++ leetcode_7.py
@@ -3,4 +3,4 @@ the reversed number'''
 class Solution:
     def reverse(self, x: int) -> int:
         result = int(str(x)[:0:-1].strip('0'))*-1 if x < 0 else int(str(x)[::-1])
-        return 0 if result.bit_length()>31 else result
+        return 0 if result.bit_length()>31 else result 
